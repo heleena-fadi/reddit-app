9165169305952f5ca4e0ff6ad2c7ace44e63221a
heleena-fadi <miss.heleena98@gmail.com>
2023-04-17 22:35:18 +0200

    create database and run server Relates #5

---
+++ src/database/config/schema.sql
@@ -0,0 +1,11 @@
+BEGIN;
+INSERT INTO users (email, password,username) values 
+()
+
+INSERT INTO posts (title, content,image, created_by, timestamp) values 
+()
+
+INSERT INTO comments (content, user_id, post_id) values 
+()
+
+COMMIT;
