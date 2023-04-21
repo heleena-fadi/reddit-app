17e6f8264f761806f19a3ecd8b901c2ddf4a8b82
heleena-fadi <miss.heleena98@gmail.com>
2023-04-21 02:08:34 +0200

    fix bugs

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,6 +4,41 @@ const email = document.querySelector("#email");
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 
+const postSignup = (data) => {
+  fetch("/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+
+    .then(() => {
+      window.location.href = "/signin";
+    })
+    .catch((err) => console.log({ err }));
+  return true;
+};
+
+const postSignin = (data) => {
+  fetch("/signin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+
+    .then((res) => {
+      localStorage.setItem("user", JSON.stringify(res));
+      window.location.href = "/profile";
+    })
+    .catch((err) => console.log({ err }));
+  return true;
+};
+
 if (signupBtn) {
   signupBtn.addEventListener("click", (e) => {
     e.preventDefault();
